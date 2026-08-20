dd034b9ad1d121ac8baedf50c65678ca3f0f7649
Markus Lanxinger <hello@plinth.it>
2026-08-20 09:43:32 +0200

    Add opt-in multi-component global mapping

--- src/pycolmap/pipeline/sfm_test.py
+++ src/pycolmap/pipeline/sfm_test.py
@@ -24,6 +24,16 @@ def test_global_pipeline_options_min_num_matches():
     assert options.min_num_matches == 20
 
 
+def test_global_pipeline_options_multiple_models():
+    options = pycolmap.GlobalPipelineOptions()
+    assert options.multiple_models is False
+    assert options.min_model_size == 3
+    options.multiple_models = True
+    options.min_model_size = 5
+    assert options.multiple_models is True
+    assert options.min_model_size == 5
+
+
 @pytest.mark.parametrize(
     "name",
     [
